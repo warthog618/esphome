549b5a9e2c8a2bf71a5e00fe130792c40f49b7b7
Kent Gibson <warthog618@gmail.com>
2025-01-07 16:17:54 +0800

    prune everything but fujitsu

--- esphome/components/atm90e32/__init__.py
+++
@@ -1,7 +0,0 @@
-import esphome.codegen as cg
-
-CODEOWNERS = ["@circuitsetup", "@descipher"]
-
-atm90e32_ns = cg.esphome_ns.namespace("atm90e32")
-
-CONF_ATM90E32_ID = "atm90e32_id"
--- esphome/components/qspi_dbi/__init__.py
+++
@@ -1,4 +0,0 @@
-CODEOWNERS = ["@clydebarrow"]
-
-CONF_DRAW_FROM_ORIGIN = "draw_from_origin"
-CONF_DRAW_ROUNDING = "draw_rounding"
